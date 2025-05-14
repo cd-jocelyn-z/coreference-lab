5f117b5a0c59ebfca0569721432dc80468ec8fb6
Joce <jocelyn.zaruma@proton.me>
2025-05-14 23:40:12 +0200

    nettoyage : suppression de scripts obsolètes

--- json_to_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-
-with open("coref_annotations.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-df = pd.DataFrame(data)
-df.to_csv("coref_annotations.csv", index=False, encoding="utf-8")
